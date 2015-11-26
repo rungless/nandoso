6ddbe15d02de93cf10ddec7de2b10b75c5313b09
Gordon Yang <rungless@outlook.com>
2015-11-26 15:29:46 +1300

    connected to database

---
+++ nandoso/nandoso/Models/Specials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace nandoso.Models
+{
+    public class Specials
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int specialsID { get; set; }
+        public string productName { get; set; }
+        public double price { get; set; }
+    }
+}
---
+++ nandoso/nandoso/Models/nandosoContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace nandoso.Models
+{
+
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))] 
+
+    public class nandosoContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public class MyConfiguration : DbMigrationsConfiguration<nandosoContext>
+        {
+            public MyConfiguration()
+            {
+                this.AutomaticMigrationsEnabled = true;
+            }
+
+            protected override void Seed(nandosoContext context)
+            {
+                var specials = new List<Specials>
+            {
+                new Specials {specialsID = 1050, productName = "Chemistry",      price = 10.00, },
+                new Specials {specialsID = 4022, productName = "Microeconomics", price = 3.00, },
+                new Specials {specialsID = 4041, productName = "Macroeconomics", price = 3.00, }
+               
+            };
+                specials.ForEach(s => context.Specials.AddOrUpdate(p => p.productName, s));
+                context.SaveChanges();
+            }
+
+        }
+
+
+
+
+        public nandosoContext() : base("name=nandosoContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<nandosoContext, MyConfiguration>());
+        }
+
+        public System.Data.Entity.DbSet<nandoso.Models.Specials> Specials { get; set; }
+    }
+}
